7596bd4323e5db96dabcd7ef376df9faa70d5890
thanhduybui <dtb1742002@gmail.com>
2026-05-20 02:14:21 +0700

    init project

---
+++ heyyou/src/main/java/com/dtb/heyyou/HeyyouApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.heyyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyyouApplication.class, args);
+	}
+
+}
---
+++ heyyou/src/test/java/com/dtb/heyyou/HeyyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtb.heyyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
